3a3178174d8289c7cb741acbe3519933468e752c
Chung Phung Phat <chphungphat@gmail.com>
2023-03-06 12:00:11 +0700

    change

---
+++ hello.js
@@ -0,0 +1,12 @@
+function showMessageAlert() {
+    alert('Xin Chào');
+}
+function showMessageId() { 
+    document.getElementById("message").innerHTML='Xin Chào';
+}
+function showMessageWrite() {
+    document.write('Xin Chào');
+}
+function showMessageLog() {
+    console.log('Xin Chào');
+}
